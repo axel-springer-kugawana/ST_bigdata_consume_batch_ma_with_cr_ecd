c4d0295f26a47558af0255089b418521410866d2
Nikola Samardzic <nikola.samardzic-ext@aviv-group.com>
2025-01-13 16:26:34 +0100

    Fixing basedata query typo

--- script/static_files/basedata_df_query.sql
+++ script/static_files/basedata_df_query.sql
@@ -12,7 +12,7 @@ FROM (
         {attributes_all_cleaned_string}
     FROM BaseDataFirst
     WHERE
-        AND (cleaned_classified_structure_building_floorNumber >= 0 or 
+        (cleaned_classified_structure_building_floorNumber >= 0 or 
             cleaned_classified_structure_building_floorNumber is NULL)
         AND (classified_management_rent_certificateOfEligibilityNeeded is null or 
             classified_management_rent_certificateOfEligibilityNeeded != 'YES')
