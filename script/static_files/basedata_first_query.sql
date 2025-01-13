f511b2c4a9cf23ce7e0643aa1dd1c6381006164a
Nikola Samardzic <nikola.samardzic-ext@aviv-group.com>
2025-01-13 16:01:00 +0100

    Fixing basedata first

--- script/static_files/basedata_first_query.sql
+++ script/static_files/basedata_first_query.sql
@@ -1,14 +1,14 @@
-with final as (
+WITH final AS (
     SELECT 
         *,
         dense_rank() OVER (PARTITION BY classified_metaData_classifiedId, to_date(classified_metaData_changeDate)
-                            ORDER BY classified_metaData_changeDate DESC, partitionChangeDate DESC) as baseRank
+                            ORDER BY classified_metaData_changeDate DESC, partitionChangeDate DESC) AS baseRank
     FROM red_red_cleaned
 )
-select *
-from final 
+SELECT *
+FROM final 
 WHERE
     baseRank = 1
-    and classified_geo_countrySpecific_de_iwtLegacyGeoID like '{geoid}%'
+    AND classified_geo_countrySpecific_de_iwtLegacyGeoID like '{geoid}%'
     AND cleanupdataproblems <= 3
-    AND cleaned_classified_distributionType == '{distribution_type}'
+    AND cleaned_classified_distributionType = '{distribution_type}'
